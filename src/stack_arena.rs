985a0cb5460a91229d93aae82682255310ef470b
Fuji <96740057+fuji-184@users.noreply.github.com>
2026-04-24 05:01:05 +0700

    Refactor StackArena to support allocator API

--- src/stack_arena.rs
+++ src/stack_arena.rs
@@ -1,40 +1,57 @@
+
 pub struct StackArena<const N: usize> {
-    val: [std::mem::MaybeUninit<u8>; N],
-    offsite: std::cell::UnsafeCell<usize>,
+    #[allow(dead_code)]
+    val: std::cell::UnsafeCell<[std::mem::MaybeUninit<u8>; N]>,
+    offset: std::cell::UnsafeCell<usize>
 }
 
 impl<const N: usize> StackArena<N> {
     #[inline(always)]
     pub fn new() -> Self {
-        const UNINIT: std::mem::MaybeUninit<u8> = std::mem::MaybeUninit::uninit();
+        let val = std::cell::UnsafeCell::new([std::mem::MaybeUninit::uninit(); N]);
         Self {
-            val: [UNINIT; N],
-            offsite: std::cell::UnsafeCell::new(0),
+            val,
+            offset: std::cell::UnsafeCell::new(0),
         }
     }
-
+    
     #[inline(always)]
-    pub fn alloc<T>(&self) -> &mut std::mem::MaybeUninit<T> {
-        unsafe {
-            let offset = self.offsite.get();
-            let align = std::mem::align_of::<T>();
-            let size = std::mem::size_of::<T>();
-            let val_ptr = self.val.as_ptr() as *mut u8;
-            let ptr = val_ptr.add(*offset);
-            let align_offset = ptr.align_offset(align);
-            let start = *offset + align_offset;
-            let end = start + size;
-            debug_assert!(end <= N, "Arena outs of memory");
-            let dst = val_ptr.add(start) as *mut std::mem::MaybeUninit<T>;
-            *offset = end;
-            &mut *dst
-        }
+    pub fn reset(&mut self) {
+        unsafe { *self.offset.get() = 0 };
     }
+}
 
+unsafe impl<const N: usize> std::alloc::Allocator for StackArena<N> {
     #[inline(always)]
-    pub fn reset(&self) {
-        unsafe {
-            *self.offsite.get() = 0;
+    fn allocate(&self, layout: std::alloc::Layout) -> Result<std::ptr::NonNull<[u8]>, std::alloc::AllocError> {
+        let base_ptr = self.val.get() as *const u8;
+        let offset = unsafe { *self.offset.get() };
+        
+        let cursor = unsafe { base_ptr.byte_add(offset) };
+        
+        let align = layout.align();
+        let size = layout.size();
+        
+        let aligned_addr = cursor.addr().checked_next_multiple_of(align)
+                .ok_or(std::alloc::AllocError)?;
+                
+        let aligned_ptr = base_ptr.with_addr(aligned_addr);
+        let end_ptr = unsafe { aligned_ptr.byte_add(size) };
+           
+        if unsafe { end_ptr.byte_offset_from(base_ptr) } as usize > N {
+            return Err(std::alloc::AllocError);
         }
+        
+        unsafe { *self.offset.get() = end_ptr.byte_offset_from(base_ptr) as usize };
+
+        Ok(unsafe {
+            std::ptr::NonNull::new_unchecked(std::ptr::slice_from_raw_parts_mut(
+                aligned_ptr.cast_mut(),
+                size,
+            ))
+        }) 
     }
+    
+    #[inline(always)]
+    unsafe fn deallocate(&self, _ptr: std::ptr::NonNull<u8>, _layout: std::alloc::Layout) { }
 }
